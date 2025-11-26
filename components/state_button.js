6e4afdc787cb984aa85dd2a5fff235d617f4aa5f
V-tito <0titovavv@gmail.com>
2025-11-26 13:11:32 +0700

    debug

--- components/state_button.js
+++ components/state_button.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import styles from "./state_button.module.css"
 
 const StateButton=({sig})=>{
@@ -9,19 +9,29 @@ const StateButton=({sig})=>{
     useEffect(()=>{
         //тут надо будет по ид платы сигнала зафетчить адрес и сделать запрос о состоянии,
         const fetchCurrentState = async ()=> { //STUB!
-        const responce = await fetch (`${process.env.API_URL}/api/river/v1/protocol/get`, {method:"GET", body: json.stringify({id:sig})})
-        console.log(`tried to get signal state with ${json.stringify({id:sig})}`)
-        const result=await responce.json
+            const api=new URL(`${process.env.API_URL}/api/river/v1/protocol/get`)
+            api.searchParams.set('id',sig)
+            const responce = await fetch (api.toString(), 
+                    {method:"GET", 
+                        //body: JSON.stringify({id:sig}),
+                        headers: {'Content-Type': 'application/json',}
+                    })
+        console.log(`tried to get signal state with ${JSON.stringify({id:sig})}`)
+        const result=await responce.json()
         setOn(result.state)
         setLastCheckTime(result.time)//todo actual key
     }
     fetchCurrentState()
-})
+},[])
 
     const changeState=async()=>{
             try {
-                const response = await fetch (`${process.env.API_URL}/api/river/v1/protocol/set`,{method:"POST", body:JSON.stringify({id:sig, currentValue:!on})})//possibly PUT
-                console.log(`tried to set signal state with ${JSON.stringify({id:sig, state:!on})}`)
+                const response = await fetch (`${process.env.API_URL}/api/river/v1/protocol/set`,
+                    {method:"POST", 
+                    body:JSON.stringify({id:sig, currentValue:!on}),
+                    headers: {'Content-Type': 'application/json',}
+                })
+                console.log(`tried to set signal state with ${JSON.stringify({id:sig, currentValue:!on})}`)
                 if (!response.ok) {
                     throw new Error (`Ошибка сети ${response.status}`)} else{setOn(!on)}
             } catch (err) {
--- components/state_indicator.js
+++ components/state_indicator.js
@@ -1,6 +1,7 @@
 "use client"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import styles from "./state_indicator.module.css"
+
 
 const StateIndicator=({sig, showCheckDisplaySettings=false, board=false})=>{
     const [on,setOn]=useState(false)
@@ -11,22 +12,28 @@ const StateIndicator=({sig, showCheckDisplaySettings=false, board=false})=>{
         
           //добавить колонку время проверки
             const fetchCurrentState = async ()=> { //STUB!
+                console.log(board)
                 let api
                 if (board) {
-                    api=`${process.env.API_URL}/api/river/v1/protocol/nop`
+                    api=new URL(`${process.env.API_URL}/api/river/v1/protocol/nop`)
                 } else {
-                    api=`${process.env.API_URL}/api/river/v1/protocol/get`
+                    api=new URL(`${process.env.API_URL}/api/river/v1/protocol/get`)
                 }
-                const responce = await fetch (api, {method:"GET", body: json.stringify({id:sig})})
-                console.log(`tried to get state on api ${api} with ${json.stringify({id:sig})}`)
-                const result=await responce.json
+                api.searchParams.set('id',sig)
+                const response = await fetch (api.toString(), 
+                    {method:"GET", 
+                        //body: JSON.stringify({id:sig}),
+                        //headers: {'Content-Type': 'application/json',}
+                    })
+                console.log(`tried to get state on api ${api.toString()}`)
+                const result= response.json()
                 
                 if (!board) {setOn(result.a)
                     setLastCheckTime(result.b)//todo actual key
                 }else {setOn(result)}
         }
         fetchCurrentState()
-    })
+    },[])
     const changeCheckSettings = () =>{
         setCheck((prev) => !prev);
     }
